d5f4b36f953f82db62381501ea6f2b828b70b6a6
WJC <petercoming_863@163.com>
2019-10-22 11:52:47 +0800

    readme

--- src/opencv stero/test_deep.py
+++ src/opencv stero/test_deep.py
@@ -12,50 +12,97 @@ cv2.createTrackbar("blockSize", "depth", 5, 255, lambda x: None)
 # camera1 = cv2.VideoCapture(0)
 # camera2 = cv2.VideoCapture(1)
 
+# def update(val=0) :
+#     stereo.setBlockSize(cv2.getTrackbarPos('window_size', 'disparity'))
+#     stereo.setUniquenessRatio(cv2.getTrackbarPos('uniquenessRatio', 'disparity'))
+#     stereo.setSpeckleWindowSize(cv2.getTrackbarPos('speckleWindowSize', 'disparity'))
+#     stereo.setSpeckleRange(cv2.getTrackbarPos('speckleRange', 'disparity'))
+#     stereo.setDisp12MaxDiff(cv2.getTrackbarPos('disp12MaxDiff', 'disparity'))
+#
+#     print('computing disparity...')
+#     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+#     cv2.imshow('left', imgL)
+#     cv2.imshow('disparity', (disp - min_disp) / num_disp)
+
+
+
 # 添加点击事件，打印当前点的距离
-def callbackFunc(e, x, y, f, p):
-    if e == cv2.EVENT_LBUTTONDOWN:
+def callbackFunc(e, x, y, f, p) :
+    if e == cv2.EVENT_LBUTTONDOWN :
         print(threeD[y][x])
 
 cv2.setMouseCallback("depth", callbackFunc, None)
 
-# while True:
+while True:
     # ret1, frame1 = camera1.read()
     # ret2, frame2 = camera2.read()
-    # 
+    #
     # if not ret1 or not ret2:
     #     break
-frame1 = cv2.imread('/Users/wangjinchao/project/PycharmProjects/Paper/master/left.png')
-frame2 = cv2.imread('/Users/wangjinchao/project/PycharmProjects/Paper/master/right.png')
-# 根据更正map对图片进行重构
-img1_rectified = cv2.remap(frame1, camera_configs.left_map1, camera_configs.left_map2, cv2.INTER_LINEAR)
-img2_rectified = cv2.remap(frame2, camera_configs.right_map1, camera_configs.right_map2, cv2.INTER_LINEAR)
+    frame1 = cv2.imread('/Users/wangjinchao/project/PycharmProjects/Paper/master/xl2.png')
+    frame2 = cv2.imread('/Users/wangjinchao/project/PycharmProjects/Paper/master/xr2.png')
+    # 根据更正map对图片进行重构
+    img1_rectified = cv2.remap(frame1, camera_configs.left_map1, camera_configs.left_map2, cv2.INTER_LINEAR)
+    img2_rectified = cv2.remap(frame2, camera_configs.right_map1, camera_configs.right_map2, cv2.INTER_LINEAR)
 
-# 将图片置为灰度图，为StereoBM作准备
-imgL = cv2.cvtColor(img1_rectified, cv2.COLOR_BGR2GRAY)
-imgR = cv2.cvtColor(img2_rectified, cv2.COLOR_BGR2GRAY)
+    # 将图片置为灰度图，为StereoBM作准备
+    imgL = cv2.cvtColor(img1_rectified, cv2.COLOR_BGR2GRAY)
+    imgR = cv2.cvtColor(img2_rectified, cv2.COLOR_BGR2GRAY)
 
-# 两个trackbar用来调节不同的参数查看效果
-num = cv2.getTrackbarPos("num", "depth")
-blockSize = cv2.getTrackbarPos("blockSize", "depth")
-if blockSize % 2 == 0:
-    blockSize += 1
-if blockSize < 5:
-    blockSize = 5
+    # 两个trackbar用来调节不同的参数查看效果
+    num = cv2.getTrackbarPos("num", "depth")
+    blockSize = cv2.getTrackbarPos("blockSize", "depth")
+    if blockSize % 2 == 0:
+        blockSize += 1
+    if blockSize < 5:
+        blockSize = 5
 
-# 根据Block Maching方法生成差异图（opencv里也提供了SGBM/Semi-Global Block Matching算法，有兴趣可以试试）
-stereo = cv2.StereoBM_create(numDisparities=16*num, blockSize=blockSize)
-disparity = stereo.compute(imgL, imgR)
+    # 根据Block Maching方法生成差异图（opencv里也提供了SGBM/Semi-Global Block Matching算法，有兴趣可以试试）
+    stereo = cv2.StereoBM_create(numDisparities=16*num, blockSize=blockSize)
+    disparity = stereo.compute(imgL, imgR)
+    disp = cv2.normalize(disparity, disparity, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    #
+    # window_size = 5
+    # min_disp = 16
+    # num_disp = 192 - min_disp
+    # blockSize = window_size
+    # uniquenessRatio = 1
+    # speckleRange = 3
+    # speckleWindowSize = 3
+    # disp12MaxDiff = 200
+    # P1 = 600
+    # P2 = 2400
+    # # imgL = cv2.imread('images/dis1.jpg')
+    # # imgR = cv2.imread('images/dis2.jpg')
+    # cv2.namedWindow('disparity')
+    # cv2.createTrackbar('speckleRange', 'disparity', speckleRange, 50, update)
+    # cv2.createTrackbar('window_size', 'disparity', window_size, 21, update)
+    # cv2.createTrackbar('speckleWindowSize', 'disparity', speckleWindowSize, 200, update)
+    # cv2.createTrackbar('uniquenessRatio', 'disparity', uniquenessRatio, 50, update)
+    # cv2.createTrackbar('disp12MaxDiff', 'disparity', disp12MaxDiff, 250, update)
+    # stereo = cv2.StereoSGBM_create(
+    #     minDisparity=min_disp,
+    #     numDisparities=num_disp,
+    #     blockSize=window_size,
+    #     uniquenessRatio=uniquenessRatio,
+    #     speckleRange=speckleRange,
+    #     speckleWindowSize=speckleWindowSize,
+    #     disp12MaxDiff=disp12MaxDiff,
+    #     P1=P1,
+    #     P2=P2
+    # )
+    # update()
 
-disp = cv2.normalize(disparity, disparity, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-# 将图片扩展至3d空间中，其z方向的值则为当前的距离
-threeD = cv2.reprojectImageTo3D(disparity.astype(np.float32)/16., camera_configs.Q)
+    # 将图片扩展至3d空间中，其z方向的值则为当前的距离
+    threeD = cv2.reprojectImageTo3D(disparity.astype(np.float32)/16., camera_configs.Q)
 
-cv2.imshow("left", img1_rectified)
-cv2.imshow("right", img2_rectified)
-cv2.imshow("depth", disp)
+    cv2.imshow("left", img1_rectified)
+    cv2.imshow("right", img2_rectified)
+    cv2.imshow("depth", disp)
 
-cv2.waitKey(0)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
 #
 #
 # camera1.release()
